0b7a4331f400eae46b19775c2f8e73212baa60f2
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2026-03-19 11:56:05 +0100

    fix emrge

---
+++ Frends.Common.Toolkit/Frends.Common.Toolkit/Attributes/RequiredIfAttribute.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Frends.Common.Toolkit.Attributes;
+
+
+[AttributeUsage(AttributeTargets.Property)]
+public class RequiredIfAttribute(string dependentProperty, params object[] targetValues) : ValidationAttribute
+{
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var field = validationContext.ObjectType.GetProperty(dependentProperty);
+        if (field == null)
+            return new ValidationResult($"Unknown property: {dependentProperty}");
+
+        var dependentValue = field.GetValue(validationContext.ObjectInstance);
+
+        if (targetValues.Contains(dependentValue))
+        {
+            if (value == null || (value is string s && string.IsNullOrWhiteSpace(s)))
+            {
+                return new ValidationResult(ErrorMessage ?? $"{validationContext.DisplayName} is required.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
